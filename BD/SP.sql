333e9e1e4c9db0910b387b4e7fbc6a6a3f27d9e6
giovanniani <giovanni_ani@hotmail.com>
2016-11-20 11:27:33 -0600

    errores

--- BD/SP.sql
+++ BD/SP.sql
@@ -1,28 +1,43 @@
-/*--------Selecciona todos ls detalles de cada producto----------*/
-/*---------------------------------------------------------------*/
+-- -----------------------------------------------------
+-- procedure seleccionar6Random
+-- -----------------------------------------------------
 
+DELIMITER $$
+USE `ferreterias`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `seleccionar6Random`()
+BEGIN
+ select idProducto,nombreProducto, descripcionProducto, precioProducto, fotoProducto FROM Producto
+ ORDER BY RAND()
+ LIMIT 6;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure seleccionarProducto
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `ferreterias`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `seleccionarProducto`(
 IN pId varchar(20)
 )
 BEGIN
 	select * from Producto
     where pId = Producto.idProducto;
-END
+END$$
 
-/*----------------Escoge 6 procutso random ----------------------*/
-/*---------------------------------------------------------------*/
+DELIMITER ;
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `seleccionar6Random`()
-BEGIN
- select idProducto,nombreProducto, descripcionProducto, precioProducto, fotoProducto FROM Producto
- ORDER BY RAND()
- LIMIT 6;
-END
+-- -----------------------------------------------------
+-- procedure seleccionarTodosProductos
+-- -----------------------------------------------------
 
-/*-------------Selecciona todos los productos------------------*/
-/*-------------------------------------------------------------*/
-
+DELIMITER $$
+USE `ferreterias`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `seleccionarTodosProductos`()
 BEGIN
  select * from Producto;
-END
+END$$
+
+DELIMITER ;
